ffb14d1452c04668669609766e88da5a68c3490b
Otrix <cashqiwi.rus@gmail.com>
2021-09-10 21:10:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,96 @@
+"use strict";
+
+
+let incr = 10,
+    decr = 10;
+
+
+// incr++;
+// decr--;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != '6');
+
+
+const isChecked = true,
+    isClose = false;
+console.log(!isChecked || isClose);
+
+
+
+
+// console.log('arr' + ' - object');
+// console.log(4 + ' - object');
+
+
+
+
+
+
+
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+
+
+// const answers = [];
+// answers[0] = prompt("Как вас зовут?", "");
+// answers[1] = prompt("Ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+// const answer = +prompt("Вам есть 18", "18");
+// console.log(answer + 5);
+
+
+
+
+// alert('Hello world');
+
+// const result = confirm("Are you here?");
+// console.log(result)
+
+
+
+// let number = 4;
+
+// console.log(-4/0);
+// console.log('str' * 9);
+
+// const person = "Alex";
+
+// const bool = true;
+// const bool1 = false;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarries: false
+// };
+
+// console.log(obj.name);
+// // console.log(obj["name"]);
+
+// let arr = ['plam.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
